2ec07a0e8f1124a53ee0e46595a7642ffe9d99c1
Young wook Jeon <ela87@naver.com>
2020-12-02 17:19:25 +0900

    Add create or update api for user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,22 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.json({
-        data: 'you hit node create-or-update-user API endpoint',
-    });
+const User = require('../models/user');
+
+exports.createOrUpdateUser = async (req, res) => {
+    const { name, picture, email } = req.user;
+
+    const user = await User.findOneAndUpdate(
+        { email: email}, { name: email.split("@")[0], picture }, { new: true }
+    );
+
+    if (user) {
+        console.log('User updated.', user)
+        res.json(user)
+    } else {
+        const newUser = await new User({
+            email,
+            name: email.split("@")[0],
+            picture,
+        }).save();
+        console.log('User created.', newUser);
+        res.json(newUser);
+    }
 };
