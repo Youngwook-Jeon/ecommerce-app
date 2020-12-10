6b55cb0d6fe9f5d316c8e6e8592767ca3913629c
Young wook Jeon <ela87@naver.com>
2020-12-10 17:14:14 +0900

    CRUD for the Category model

--- server/routes/category.js
+++ server/routes/category.js
@@ -9,7 +9,7 @@ const { authCheck, adminCheck } = require('../middlewares/auth');
 const { create, read, update, remove, list } = require('../controllers/category');
 
 router.post('/category', authCheck, adminCheck, create);
-router.get('/category/:slug', authCheck, adminCheck, read);
+router.get('/category/:slug', read);
 router.get('/categories', list);
 router.put('/category/:slug', authCheck, adminCheck, update);
 router.delete('/category/:slug', authCheck, adminCheck, remove);
