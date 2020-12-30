e3e78897b4b1a394d2cbe2e282d8a3e887fa7b73
Young wook Jeon <ela87@naver.com>
2020-12-31 01:56:46 +0900

    Api for product delete

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,27 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AdminNav from "../../components/nav/AdminNav";
-import { getProductsByCount } from "../../functions/product";
 
 const AdminDashboard = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  const loadAllProducts = () => {
-    setLoading(true);
-    getProductsByCount(100)
-      .then((res) => {
-        setProducts(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setLoading(false);
-        console.log(err);
-      });
-  };
 
   return (
     <div className="container-fluid">
@@ -29,12 +9,9 @@ const AdminDashboard = () => {
         <div className="col-md-2">
           <AdminNav />
         </div>
-        {loading ? (
-          <h4 className="text-danger">Loading...</h4>
-        ) : (
-          <h4>All Products</h4>
-        )}
-        <div className="col">{JSON.stringify(products)}</div>
+        <div className="col">
+          <h4>Admin Dashboard</h4>
+        </div>
       </div>
     </div>
   );
