0fe7f0f83021d87c6ac48183e75087e83460aec3
Young wook Jeon <ela87@naver.com>
2021-01-15 02:26:31 +0900

    Single Product Page

---
+++ client/src/components/cards/SingleProduct.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Card, Tabs } from "antd";
+import { Link } from "react-router-dom";
+import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import Laptop from "../../images/laptop.png";
+import ProductListItems from "./ProductListItems";
+
+const { TabPane } = Tabs;
+
+const SingleProduct = ({ product }) => {
+  const { title, images, description } = product;
+  return (
+    <div>
+      <div className="col-md-7">
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay infiniteLoop>
+            {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+          </Carousel>
+        ) : (
+          <Card
+            cover={<img src={Laptop} className="mb-3 card-image" alt="" />}
+          ></Card>
+        )}
+
+        <Tabs type="card">
+            <TabPane tab="Description" key="1">
+                {description && description}
+            </TabPane>
+            <TabPane tab="More" key="2">
+                Call us on xxx xxxx xxxx to learn more about this product.
+            </TabPane>
+        </Tabs>
+      </div>
+
+      <div className="col-md-5">
+        <h1 className="bg-info p-3">{title}</h1>
+        <Card
+          actions={[
+            <div>
+              <ShoppingCartOutlined className="text-success" />
+              <br />
+              Add to Cart
+            </div>,
+            <Link to="/">
+              <HeartOutlined className="text-info" />
+              <br />
+              Add to Wishlist
+            </Link>,
+          ]}
+        >
+          <ProductListItems product={product} />
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProduct;
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getProduct } from "../functions/product";
+import SingleProduct from "../components/cards/SingleProduct";
 
 const Product = ({ match }) => {
   const [product, setProduct] = useState({});
@@ -11,7 +12,20 @@ const Product = ({ match }) => {
 
   const loadSingleProduct = () =>
     getProduct(slug).then((res) => setProduct(res.data));
-  return <div>{JSON.stringify(product)}</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row pt-4">
+        <SingleProduct product={product} />
+      </div>
+      <div className="row">
+        <div className="col text-center pt-5 pb-5">
+          <hr />
+          <h4>Related Products</h4>
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
