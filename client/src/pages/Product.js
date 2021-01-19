a264d5654c944a5951c0898f9f79927efc8259f0
Young wook Jeon <ela87@naver.com>
2021-01-19 23:24:24 +0900

    API Request with Rating

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,21 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { getProduct } from "../functions/product";
+import { getProduct, productStar } from "../functions/product";
 import SingleProduct from "../components/cards/SingleProduct";
+import { useSelector } from 'react-redux';
 
 const Product = ({ match }) => {
   const [product, setProduct] = useState({});
+  const [star, setStar] = useState(0);
+
+  const { user } = useSelector(state => ({ ...state }));
+
   const { slug } = match.params;
 
   useEffect(() => {
     loadSingleProduct();
   }, [slug]);
 
+  useEffect(() => {
+    if (product.ratings && user) {
+      let existingRatingObject = product.ratings.find(
+        (elm) => elm.postedBy.toString() === user._id.toString()
+      );
+      existingRatingObject && setStar(existingRatingObject.star); // current user's star
+    }
+  })
+
   const loadSingleProduct = () =>
     getProduct(slug).then((res) => setProduct(res.data));
+
+  const onStarClick = (newRating, name) => {
+    setStar(newRating);
+    productStar(name, newRating, user.token)
+      .then(res => {
+        console.log(res.data);
+        loadSingleProduct();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="container-fluid">
       <div className="row pt-4">
-        <SingleProduct product={product} />
+        <SingleProduct product={product} onStarClick={onStarClick} star={star} />
       </div>
       <div className="row">
         <div className="col text-center pt-5 pb-5">
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -16,16 +16,28 @@ const Login = ({ history }) => {
     const { user } = useSelector(state => ({ ...state }));
 
     useEffect(() => {
-        if (user && user.token) history.push("/");
+        let intended = history.location.state;
+        if (intended) {
+            return;
+        } // if intended, roleBasedRedirect will handle it
+        else {
+            if (user && user.token) history.push("/");
+        }
     }, [user, history]);
 
     let dispatch = useDispatch();
 
     const roleBasedRedirect = (res) => {
-        if (res.data.role === 'admin') {
-            history.push('/admin/dashboard');
+        // check if intended
+        let intended = history.location.state;
+        if (intended) {
+            history.push(intended.from);
         } else {
-            history.push('/user/history');
+            if (res.data.role === 'admin') {
+                history.push('/admin/dashboard');
+            } else {
+                history.push('/user/history');
+            }
         }
     };
 
