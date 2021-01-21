16cf55851f136f5285b51059e4d655f0ea3a926f
Young wook Jeon <ela87@naver.com>
2021-01-21 23:38:12 +0900

    Show Products based on its Category

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,8 @@ import ProductCreate from './pages/admin/product/ProductCreate';
 import AllProducts from './pages/admin/product/AllProducts';
 import ProductUpdate from './pages/admin/product/ProductUpdate';
 import Product from './pages/Product';
+import CategoryHome from './pages/category/CategoryHome';
+import SubHome from './pages/sub/SubHome';
 
 import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
@@ -81,6 +83,8 @@ const App = () => {
         <AdminRoute exact path="/admin/products" component={AllProducts} />
         <AdminRoute exact path="/admin/product/:slug" component={ProductUpdate} />
         <Route exact path="/product/:slug" component={Product} />
+        <Route exact path="/category/:slug" component={CategoryHome} />
+        <Route exact path="/sub/:slug" component={SubHome} />
       </Switch>
     </div>
   );
--- server/controllers/sub.js
+++ server/controllers/sub.js
@@ -1,44 +1,52 @@
-const Sub = require('../models/sub');
-const slugify = require('slugify');
+const Sub = require("../models/sub");
+const Product = require("../models/product");
+const slugify = require("slugify");
 
 exports.create = async (req, res) => {
-    try {
-        const { name, parent } = req.body;
-        const sub = await new Sub({ name, slug: slugify(name), parent }).save();
-        res.json(sub);
-    } catch (err) {
-        res.status(400).send('Create a SubCategory failed.');
-    }
+  try {
+    const { name, parent } = req.body;
+    const sub = await new Sub({ name, slug: slugify(name), parent }).save();
+    res.json(sub);
+  } catch (err) {
+    res.status(400).send("Create a SubCategory failed.");
+  }
 };
 
 exports.list = async (req, res) => {
-    res.json(await Sub.find({}).sort({ createdAt: -1 }).exec());
+  res.json(await Sub.find({}).sort({ createdAt: -1 }).exec());
 };
 
 exports.read = async (req, res) => {
-    let sub = await Sub.findOne({ slug: req.params.slug }).exec();
-    res.json(sub);
+  let sub = await Sub.findOne({ slug: req.params.slug }).exec();
+  const products = await Product.find({ subs: sub })
+    .populate("category")
+    .exec();
+
+  res.json({
+      sub,
+      products,
+  });
 };
 
 exports.update = async (req, res) => {
-    const { name, parent } = req.body;
-    try {
-        const updated = await Sub.findOneAndUpdate(
-            { slug: req.params.slug }, 
-            { name, parent, slug: slugify(name) },
-            { new: true }
-        );
-        res.json(updated);
-    } catch (err) {
-        res.status(400).send("Update the SubCategory failed.")
-    }
+  const { name, parent } = req.body;
+  try {
+    const updated = await Sub.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, parent, slug: slugify(name) },
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(400).send("Update the SubCategory failed.");
+  }
 };
 
 exports.remove = async (req, res) => {
-    try {
-        const deleted = await Sub.findOneAndDelete({ slug: req.params.slug });
-        res.json(deleted);
-    } catch (err) {
-        res.status(400).send('Delete the SubCategory failed.');
-    }
+  try {
+    const deleted = await Sub.findOneAndDelete({ slug: req.params.slug });
+    res.json(deleted);
+  } catch (err) {
+    res.status(400).send("Delete the SubCategory failed.");
+  }
 };
