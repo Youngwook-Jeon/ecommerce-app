abec088085d0f305ac59096be70bdf0abf293670
Young wook Jeon <ela87@naver.com>
2021-02-02 20:17:02 +0900

    Remove/Update Products in Cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,20 +1,145 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import ModalImage from "react-modal-image";
+import laptop from "../../images/laptop.png";
+import { toast } from "react-toastify";
+import { CheckCircleOutlined, CloseCircleOutlined, CloseOutlined } from "@ant-design/icons";
 
 const ProductCardInCheckout = ({ product }) => {
-    return (
-        <tbody>
-            <tr>
-                <td>Image</td>
-                <td>{product.title}</td>
-                <td>{product.price}</td>
-                <td>{product.brand}</td>
-                <td>{product.color}</td>
-                <td>{product.count}</td>
-                <td>Shipping Icon</td>
-                <td>Delete Icon</td>
-            </tr>
-        </tbody>
-    );
+  const colors = ["Black", "Brown", "Silver", "White", "Blue"];
+  let dispatch = useDispatch();
+
+  const handleColorChange = (e) => {
+    let cart = [];
+
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+
+      cart.map((p, i) => {
+        if (p._id === product._id) {
+          cart[i].color = e.target.value;
+        }
+      });
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
+  const handleQuantityChange = (e) => {
+    let count = e.target.value < 1 ? 1 : e.target.value;
+
+    if (count > product.quantity) {
+      toast.error(`Max available quantity: ${product.quantity}`);
+      return;
+    }
+    let cart = [];
+
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+
+      cart.map((p, i) => {
+        if (p._id === product._id) {
+          cart[i].count = count;
+        }
+      });
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
+  const handleRemove = () => {
+    let cart = [];
+
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+
+      cart.map((p, i) => {
+        if (p._id === product._id) {
+          cart.splice(i, 1);
+        }
+      });
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
+  return (
+    <tbody>
+      <tr>
+        <td>
+          <div style={{ width: "100px", height: "auto" }}>
+            {product.images.length ? (
+              <ModalImage
+                small={product.images[0].url}
+                large={product.images[0].url}
+              />
+            ) : (
+              <ModalImage small={laptop} large={laptop} />
+            )}
+          </div>
+        </td>
+        <td>{product.title}</td>
+        <td>{product.price}</td>
+        <td>{product.brand}</td>
+        <td>
+          <select
+            onChange={handleColorChange}
+            name="color"
+            className="form-control"
+          >
+            {product.color ? (
+              <option value={product.color}>{product.color}</option>
+            ) : (
+              <option>Select</option>
+            )}
+            {colors
+              .filter((c) => c !== product.color)
+              .map((c) => (
+                <option key={c} value={c}>
+                  {c}
+                </option>
+              ))}
+          </select>
+        </td>
+        <td className="text-center">
+          <input
+            type="number"
+            className="form-control"
+            value={product.count}
+            onChange={handleQuantityChange}
+          />
+        </td>
+        <td className="text-center">
+          {product.shipping === "Yes" ? (
+            <CheckCircleOutlined className="text-success" />
+          ) : (
+            <CloseCircleOutlined className="text-danger" />
+          )}
+        </td>
+        <td className="text-center">
+            <CloseOutlined onClick={handleRemove} className="text-danger pointer" />
+        </td>
+      </tr>
+    </tbody>
+  );
 };
 
 export default ProductCardInCheckout;
